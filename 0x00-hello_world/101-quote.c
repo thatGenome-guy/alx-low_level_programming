b4d3ed24bac13775291bf32b279551027bf361a2
Oduola David Ifeoluwa <davidoduola001@gmail.com>
2023-03-16 22:11:37 -0700

    UNIX Operating

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  * main-  a C program that prints exactly and that piece of art is useful" -
+  *main - C program that prints exactly and that piece of art is useful" -
   *Dora Korpar, 2015-10-19, followed by a new line, to the standard error
-  * Return: 1 (Success)
+  *Return: 1 (Success)
   */
 int main(void)
 {
