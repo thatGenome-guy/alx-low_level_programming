91ca7d78cbb55c8c48a4226d1f908e4b4a1935bc
Oduola David Ifeoluwa <davidoduola001@gmail.com>
2023-03-16 21:56:50 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - print quote
  *
- * Return: 1 (Success)
+ * Description: print Dora's quote
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
