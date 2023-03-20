69dff75c90635a37fc2ca2fe033058f25b462f9c
Oduola David Ifeoluwa <davidoduola001@gmail.com>
2023-03-20 15:41:52 -0700

    Hexadecimals

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,11 @@
 
 int main(void)
 {
-	char i;
+	int i;
 	char j;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i + '0')
-
+		putchar(i + '0');
 	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
